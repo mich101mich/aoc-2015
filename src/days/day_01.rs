ad8bc50ef431dc12ff4730e9b3e5b745ef4453be
mich101mich <mich101mich@gmail.com>
2022-06-26 14:05:46 +0200

    added day 01

--- src/days/day_01.rs
+++ src/days/day_01.rs
@@ -4,19 +4,30 @@ use crate::utils::*;
 pub fn run() {
     #[allow(unused_variables)]
     let input = include_str!("../input/01.txt");
-    // let input = ;
-    
+
+    let mut floor = 0;
     let parsed = input
-        //.lines()
-        //.chars()
-        //.map(|l| l.chars().to_vec())
-        //.map(parse)
-        //.map(|l| scanf!(l, "{}", isize).unwrap())
-        //.to_vec()
-        //.sum::<isize>()
-        //.parse::<isize>()
-        ;
-    
-    //pv!(parsed);
-    
+        .chars()
+        .map(|c| if c == '(' { 1 } else { -1 })
+        .position(|i| {
+            floor += i;
+            floor == -1
+        })
+        .unwrap()
+        + 1;
+
+    pv!(parsed);
+}
+
+#[allow(unused)]
+pub fn part_one() {
+    #[allow(unused_variables)]
+    let input = include_str!("../input/01.txt");
+
+    let parsed = input
+        .chars()
+        .map(|c| if c == '(' { 1 } else { -1 })
+        .sum::<i32>();
+
+    pv!(parsed);
 }
